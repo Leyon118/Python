81598cd426a7a1a325154aedba0ee6636ea9df75
leyon118 <leyonlboss@hotmail.com>
2022-12-08 15:28:48 +0000

    Sample Program

--- Unit01/calc.py
+++ Unit01/calc.py
@@ -1,11 +1,29 @@
 d_lis=[]
-d_rec=int(input('Howmany numbersdo you want to enter?: '))
+d_rec=int(input('How many numbers do you want to enter?: '))
 
 i = 0
 while i<d_rec:
     num=int(input('Enter a whole digit: '))
     d_lis.append(num)
     i+=1
-    
+
+d_ser=input('Would you like to multiply(m) or divide(d)?')
+
+#for number in d_lis:
+
+mul = 1
+div = 1
+if d_ser.lower()=='m':
+    for x in d_lis:
+        mul *= x
+    print(mul)
+elif d_ser.lower()=='d':
+    for x in d_lis:
+        div /= x
+    print(div)
+else:
+        print('Option unavalable')
+
 
 print(d_lis)
+
