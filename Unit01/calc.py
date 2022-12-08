54051b0dd243bd67a7f786fd10cb9d516e3751b9
leyon118 <leyonlboss@hotmail.com>
2022-12-08 12:19:53 +0000

    Com 2

---
+++ Unit01/calc.py
@@ -0,0 +1,11 @@
+d_lis=[]
+d_rec=int(input('Howmany numbersdo you want to enter?: '))
+
+i = 0
+while i<d_rec:
+    num=int(input('Enter a whole digit: '))
+    d_lis.append(num)
+    i+=1
+    
+
+print(d_lis)
